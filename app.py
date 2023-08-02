3bd3b2dc80e53097b750f02e7a6a0b91b341ab0c
WolfyCodeK <45407550+WolfyCodeK@users.noreply.github.com>
2023-08-02 11:37:02 +0100

    Added constants where needed

--- app.py
+++ app.py
@@ -4,70 +4,82 @@ import json
 import re
 import pyperclip
 
-"""
-    [sg.Text('Enter student name'), sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')],
-            
-    sg.Button('', button_color=(sg.theme_background_color(), sg.theme_background_color()), image_filename='res\cog.png', size=(512, 512), image_subsample=15, border_width=0)
-    
-     sg.set_options(suppress_raise_key_errors=False, suppress_error_popups=True, suppress_key_guessing=True)
-"""
-
-# All the stuff inside your window.
+# Custom Fonts
 titleFont = ("Courier New", 15)
 textFont = ("Courier New", 12)
 
+# File Paths
+TEMPLATES_PATH = 'templates.json'
+
+# Resource Paths
+BLANK_ICO = 'res\Blank.ico'
+MAIN_ICO = 'res\Moneybox.ico'
+
+# Window Sizes
+MAIN_WIDTH = 600
+MAIN_HEIGHT = 225
+SELECT_WIDTH = 300
+SELECT_HEIGHT = 160
+
+# Element Sizes
 PADDING = 15
 
-# Elements IDs
+# Button Values
 EDIT_BUTTON = 'Edit'
 EXIT_BUTTON = 'Exit'
-NAMES_COMBOBOX = 'Names'
-INPUT_SIZE = 15
 NEW_BUTTON = 'New Template'
 SAVE_BUTTON = 'Save & Close'
+
+# Combo Values
+NAMES_COMBOBOX = 'Names'
+
+# Input Values
+RECIPIENT_INPUT = 'Recipient'
+NUMBER_INPUT = 'Number'
+COST_INPUT = 'Cost'
+INPUT_SIZE = 15
 
 def selectedTemplateWindow(isNewTemplate, name):
     with open(TEMPLATES_PATH, 'r') as f:
         jsonData = json.load(f)
     
     if (isNewTemplate):
-        layout = [[sg.Text('Recipient', font=textFont), sg.Input(size=INPUT_SIZE*2, font=textFont, key=('Recipient'))],
-              [sg.Text('Number of lessons', font=textFont), sg.Input(size=INPUT_SIZE, font=textFont, key=('Number'))],
-              [sg.Text('Cost of lesson  £', font=textFont), sg.Input(size=INPUT_SIZE, font=textFont, key=('Cost'))], 
+        layout = [[sg.Text('Recipient', font=textFont), sg.Input(size=INPUT_SIZE*2, font=textFont, key=(RECIPIENT_INPUT))],
+              [sg.Text('Number of lessons', font=textFont), sg.Input(size=INPUT_SIZE, font=textFont, key=(NUMBER_INPUT))],
+              [sg.Text('Cost of lesson  £', font=textFont), sg.Input(size=INPUT_SIZE, font=textFont, key=(COST_INPUT))], 
               [sg.VPush()],
               [sg.Button(SAVE_BUTTON, font=textFont), sg.Push(), sg.Button(EXIT_BUTTON, font=textFont)]
               ]
     else:
-        layout = [[sg.Text('Recipient', font=textFont), sg.Input(size=INPUT_SIZE*2, font=textFont, key=('Recipient'), disabled=True, default_text=name, disabled_readonly_background_color='DarkRed')],
-              [sg.Text('Number of lessons', font=textFont), sg.Input(size=INPUT_SIZE, font=textFont, key=('Number'), default_text=jsonData[name]['Number'])],
-              [sg.Text('Cost of lesson  £', font=textFont), sg.Input(size=INPUT_SIZE, font=textFont, key=('Cost'), default_text=jsonData[name]['Cost'])], 
+        layout = [[sg.Text('Recipient', font=textFont), sg.Input(size=INPUT_SIZE*2, font=textFont, key=(RECIPIENT_INPUT), disabled=True, default_text=name, disabled_readonly_background_color='DarkRed')],
+              [sg.Text('Number of lessons', font=textFont), sg.Input(size=INPUT_SIZE, font=textFont, key=(NUMBER_INPUT), default_text=jsonData[name][NUMBER_INPUT])],
+              [sg.Text('Cost of lesson  £', font=textFont), sg.Input(size=INPUT_SIZE, font=textFont, key=(COST_INPUT), default_text=jsonData[name][COST_INPUT])], 
               [sg.VPush()],
               [sg.Button(SAVE_BUTTON, font=textFont), sg.Push(), sg.Button(EXIT_BUTTON, font=textFont)]
               ] 
     
     sg.Titlebar()
     
-    window = sg.Window('', layout, element_justification='c', size=(300, 160), modal=True, disable_close=True, icon='res\Blank.ico')
+    window = sg.Window('', layout, element_justification='c', size=(SELECT_WIDTH, SELECT_HEIGHT), modal=True, disable_close=True, icon=BLANK_ICO)
     
     while True:
         event, values = window.read()
         if event == EXIT_BUTTON:
             break
         if event == SAVE_BUTTON: 
-            if re.search('\d', values['Recipient']):
-                sg.popup('Recipient name cannot contain numbers!', title='', font=textFont, icon='res\Blank.ico')
-            elif re.search('\D', values['Number']):
-                sg.popup('Number of lessons cannot contain characters!', title='', font=textFont, icon='res\Blank.ico')
-            elif re.search('\D', str(values['Cost']).replace('.', '')):
-                sg.popup('Cost of lesson cannot contain characters!', title='', font=textFont, icon='res\Blank.ico')
-            elif len(str(values['Recipient'])) == 0 or len(str(values['Number'])) == 0 or len(str(values['Cost'])) == 0:
-                sg.popup('All fields must be completed!', title='', font=textFont, icon='res\Blank.ico')
+            if re.search('\d', values[RECIPIENT_INPUT]):
+                sg.popup('Recipient name cannot contain numbers!', title='', font=textFont, icon=BLANK_ICO)
+            elif re.search('\D', values[NUMBER_INPUT]):
+                sg.popup('Number of lessons cannot contain characters!', title='', font=textFont, icon=BLANK_ICO)
+            elif re.search('\D', str(values[COST_INPUT]).replace('.', '')):
+                sg.popup('Cost of lesson cannot contain characters!', title='', font=textFont, icon=BLANK_ICO)
+            elif len(str(values[RECIPIENT_INPUT])) == 0 or len(str(values[NUMBER_INPUT])) == 0 or len(str(values[COST_INPUT])) == 0:
+                sg.popup('All fields must be completed!', title='', font=textFont, icon=BLANK_ICO)
             else:
                 name = values['Recipient']
                 
                 if (name in jsonData) and (isNewTemplate == True):
-                    sg.popup('Template with that name already exists!', title='', font=textFont, icon='res\Blank.ico')
+                    sg.popup('Template with that name already exists!', title='', font=textFont, icon=BLANK_ICO)
                 else:
                     info = {
                         'Number' : values['Number'],
@@ -109,7 +121,7 @@ def mainWindow():
                 
             ]
 
-    window = sg.Window('Invoice Templates', layout, element_justification='l', size=(600, 225), icon='res\Moneybox.ico')
+    window = sg.Window('Invoice Templates', layout, element_justification='l', size=(MAIN_WIDTH, MAIN_HEIGHT), icon=MAIN_ICO)
     
     # Event Loop to process "events" and get the "values" of the inputs
     while True:
@@ -138,18 +150,19 @@ def mainWindow():
                 
             window[NAMES_COMBOBOX].update(values=namesList)
         if event == 'Subject':
-            print(jsonData[values[NAMES_COMBOBOX]]['Number'])
+            print(jsonData[values[NAMES_COMBOBOX]][NUMBER_INPUT])
         if event == 'Body':
-            numberOfLessons = str(jsonData[values[NAMES_COMBOBOX]]['Number'])
-            costOfLessons = str(jsonData[values[NAMES_COMBOBOX]]['Cost'])
+            numberOfLessons = str(jsonData[values[NAMES_COMBOBOX]][NUMBER_INPUT])
+            costOfLessons = str(jsonData[values[NAMES_COMBOBOX]][COST_INPUT])
             totalCost = str(int(numberOfLessons) * float(costOfLessons))
+            totalCost = '%.2f' % (round(float(totalCost), 2))
             pyperclip.copy("""Hi """ + str(values[NAMES_COMBOBOX]) +  """,
 
 Here is my invoice for Melody's drum lessons 2nd half summer term 2023.
 --------
 There are """ + numberOfLessons + """ sessions this 2nd half summer term from Tuesday 6th June to and including Tuesday 18th July.
  
-""" + numberOfLessons + """ x £ """ + costOfLessons + """ = £ """ + totalCost + """
+""" + numberOfLessons + """ x £""" + costOfLessons + """ = £""" + totalCost + """
 
 Thank you
 --------
@@ -161,15 +174,10 @@ Kind regards""")
 
     window.close()
 
-if __name__ == "__main__":
-    
-    TEMPLATES_PATH = 'templates.json'
-    
+if __name__ == "__main__":    
     if (not os.path.isfile(TEMPLATES_PATH)):
         f = open(TEMPLATES_PATH, 'w')
 
     sg.theme('DarkAmber')
 
     mainWindow()
-    
-    
