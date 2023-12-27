bedbc48c4d0011be18ee1c3dbc097f84d652106b
WolfyCodeK <45407550+WolfyCodeK@users.noreply.github.com>
2023-12-27 09:48:12 +0000

    added folders to move_files script

--- move_files.sh
+++ move_files.sh
@@ -8,10 +8,18 @@ mv _* lib/
 
 # Move folders named 'tcl' and 'tk' into the 'lib' folder
 mv tcl lib/
+mv tcl8 lib/
 mv tk lib/
+mv certifi lib/
+mv google_api_core-1.34.0.dist-info lib/
+mv google_api_python_client-1.8.0.dist-info lib/
+mv httplib2 lib/
 
 # Move files ending with .pyd into the 'lib' folder
 mv *.pyd lib/
+
+# Move all files ending in .py into the 'lib' folder
+mv *.py lib/
 
 # Move all files ending in .dll unless they start with 'python3' into the 'lib' folder
 shopt -s extglob
